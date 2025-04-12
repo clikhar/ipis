f6befd9786418f0bbdfe2ca2cbb1d43fde2bf682
clikhar <clikhar@gmail.com>
2025-04-12 16:34:38 +0530

    To add all Hosts

--- ntes/settings.py
+++ ntes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1bkcxw$7mmt*i2w8#7m(4k41s@__j4if+uaodjw9tci7npxz0b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.2']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.1.2','*']
 
 
 # Application definition
